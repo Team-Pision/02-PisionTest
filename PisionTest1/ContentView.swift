be7704a69278345a89a583ced8d4c4427a8e9613
YeoSeongil <seongil_yeo@naver.com>
2025-07-09 14:35:23 +0900

    commit

---
+++ PisionTest1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PisionTest1
+//
+//  Created by 여성일 on 7/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+          Text("12")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PisionTest1/PisionTest1App.swift
@@ -0,0 +1,17 @@
+//
+//  PisionTest1App.swift
+//  PisionTest1
+//
+//  Created by 여성일 on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct PisionTest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
