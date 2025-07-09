b00177f03671674d298d411257abce1784540ae0
YeoSeongil <seongil_yeo@naver.com>
2025-07-09 14:44:11 +0900

    [FEAT] 커스텀 카메라 구현

---
+++ PisionTest1/View/Feature/Main/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  PisionTest1
+//
+//  Created by 여성일 on 7/9/25.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
